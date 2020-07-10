6d6505807aa0fac71246560aefe28a546f37d219
obsfx <balandiomer@gmail.com>
2020-07-10 05:39:03 +0300

    entrydetails listing added

--- src/app/scenes/EntryDetailsScene.ts
+++ src/app/scenes/EntryDetailsScene.ts
@@ -1,5 +1,6 @@
 import Scene from './Scene';
 import TitleScene from './TitleScene';
+import BulkQueueScene from './BulkQueueScene';
 
 import { 
     UITypes,
@@ -11,6 +12,8 @@ import {
 } from '../../ui';
 
 import { EntryDetailsSceneOptions } from '../action-templates';
+
+import { ResultsSceneActionIDS } from '../action-ids';
 
 import App, { Entry } from '../App';
 import Entries from '../modules/Entries';
@@ -31,7 +34,9 @@ export default abstract class EntryDetailsScene extends Scene {
     }
 
     public static hide(): void {
+        Terminal.showCursor();
         TitleScene.hide();
+        BulkQueueScene.hide();
         EventHandler.detachResizeReRenderEventMap(0, 'EntryDetailsScene');
         this.list.hide();
     }
@@ -39,6 +44,7 @@ export default abstract class EntryDetailsScene extends Scene {
     public static render(): void {
         this.ymargin = 0;
 
+        Terminal.hideCursor();
         TitleScene.show();
 
         Terminal.cursorXY(1, 5);
@@ -48,12 +54,14 @@ export default abstract class EntryDetailsScene extends Scene {
             console.log(detail);
         }
 
+        BulkQueueScene.show(1, 6 + this.entryDetailsText.length + this.ymargin);
+        BulkQueueScene.updateQueueLen(Object.keys(App.state.bulkQueue).length);
 
         let options: UITypes.Listing[] = EntryDetailsSceneOptions.map(
             (e: UITypes.ComponentParams) => (
                 new Listing({
                     title: e.title,
-                    value: e.value,
+                    value: this.selectedEntry.ID,
                     actionID: e.actionID,
                     color: e.color,
                     hovercolor: e.hovercolor
@@ -63,8 +71,31 @@ export default abstract class EntryDetailsScene extends Scene {
 
         this.list.setXY(2, 7 + this.entryDetailsText.length + this.ymargin);
         this.list.attachListingArr(options, options.length);
+
+        if (App.state.bulkQueue[this.selectedEntry.ID]) {
+            this.list.applyCheckedStyle(true);
+        }
+
         this.list.show();
         this.list.showInfo();
+
+        this.list.attachListingOnReturnFn((list: List) => {
+            let currentListing: UITypes.ReturnObject = list.getCurrentListing();
+
+            if (currentListing.actionID == ResultsSceneActionIDS.ADD_TO_BULK_DOWNLOADING_QUEUE) {
+                if (App.state.bulkQueue[currentListing.value]) {
+                    delete App.state.bulkQueue[currentListing.value];
+                    list.toggleChecked(false);
+                } else {
+                    App.state.bulkQueue[currentListing.value] = true;
+                    list.toggleChecked(true);
+                }
+
+                BulkQueueScene.updateQueueLen(Object.keys(App.state.bulkQueue).length);
+            } else {
+                list.terminateAwaiting = true;
+            }
+        });
     }
 
     public static onResize(): void {
--- src/ui/components/List.ts
+++ src/ui/components/List.ts
@@ -2,9 +2,15 @@ import { Types } from '../types.namespace';
 
 import ListingContainer from './ListingContainer';
 
+import keymap from '../keymap';
+
 export default class List extends ListingContainer {
+    listingOnReturnFn: Function | null;
+
     constructor(zindex: number = 0) {
         super(zindex);
+
+        this.listingOnReturnFn = null;
     }
 
     public render(): void {
@@ -18,5 +24,54 @@ export default class List extends ListingContainer {
         }
 
         this.renderCursor();
+    }
+
+    public eventHandler(key: Types.stdinOnKeyParam): void {
+        if (key.name == keymap.PREVLISTING) {
+            this.prev();
+            this.render();
+        }
+
+        if (key.name == keymap.NEXTLISTING) {
+            this.next();
+            this.render();
+        }
+
+        if (key.name == keymap.DOACTION && this.activeAwait) {
+            if (this.listingOnReturnFn) {
+                this.listingOnReturnFn(this);
+            } else {
+                this.terminateAwaiting = true;
+            }
+        }
+    }
+
+    public attachListingOnReturnFn(fn: Function): void {
+        this.listingOnReturnFn = fn;
+    }
+
+    public detachListingOnReturnFn(): void {
+        this.listingOnReturnFn = null;
+    }
+
+    public applyCheckedStyle(checked: boolean): void {
+        let oldText: string = !checked ?
+            'Remove from Bulk Downloading Queue' :
+            'Add to Bulk Downloading Queue';
+        
+        let newText: string = checked ?
+            'Remove from Bulk Downloading Queue' :
+            'Add to Bulk Downloading Queue';
+
+        for (let i: number = 0; i < this.renderingQueue.length; i++) {
+            if (this.renderingQueue[i].text.text == oldText) {
+                this.renderingQueue[i].text.setText(newText);
+            }
+        }
+    }
+
+    public toggleChecked(checked: boolean = false): void {
+        this.applyCheckedStyle(checked);
+        this.show();
     }
 }
