4bbf84c2118615f8bd618bde32e518668ea606a0
obsfx <balandiomer@gmail.com>
2020-07-15 02:40:44 +0300

    gh-pages

--- src/app/constants.ts
+++
@@ -1,8 +0,0 @@
-import CONFIG from './config';
-
-export default {
-    STRING_REPLACE_REGEX: /(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,
-
-    MD5_REQ_PATTERN: `${CONFIG.MIRROR}json.php?ids={ID}&fields=md5`,
-    MD5_DOWNLOAD_PAGE_PATTERN: `${CONFIG.DOWNLOAD_MIRROR}main/{MD5}`,
-}
