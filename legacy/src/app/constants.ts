3c74301ebda1c7615ae9f400dd709cab83f52590
Ömercan Balandı <balandiomer@gmail.com>
2020-11-14 02:32:09 +0300

    Ignored: Legacy

--- legacy/src/app/constants.ts
+++
@@ -1,8 +0,0 @@
-import CONFIG from './config';
-
-export default {
-    STRING_REPLACE_REGEX: /(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,
-
-    MD5_REQ_PATTERN: `${CONFIG.MIRROR}json.php?ids={ID}&fields=md5`,
-    MD5_SEARCH_PATTERN: `${CONFIG.MIRROR}search.php?req={MD5}&column=md5`
-}
--- legacy/src/bulk-downloader/types.namespace.ts
+++
@@ -1,3 +0,0 @@
-export namespace Types {
-    export type bulkDownloadMode = 'ID' | 'MD5';
-}
