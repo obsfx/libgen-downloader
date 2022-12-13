f19e82df1a610b6fe45010602eaff86e481d7d86
obsfx <balandiomer@gmail.com>
2022-12-13 08:59:17 +0300

    Add usage info component

--- src/tui/layouts/detail/index.tsx
+++ src/tui/layouts/detail/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Box } from "ink";
 import ContentContainer from "../../components/ContentContainer";
 import { useAppContext } from "../../contexts/AppContext";
 import DetailRow from "./DetailRow";
 import DetailEntryOptions from "./DetailEntryOptions";
+import UsageInfo from "../../components/UsageInfo";
 
 const Detail: React.FC = () => {
   const { detailedEntry } = useAppContext();
@@ -12,18 +14,21 @@ const Detail: React.FC = () => {
   }
 
   return (
-    <ContentContainer>
-      {Object.entries(detailedEntry)
-        .filter(([key]) => key !== "downloadUrls")
-        .map(([key, value], idx) => (
-          <DetailRow
-            key={idx}
-            label={key === "id" ? key.toUpperCase() : `${key[0].toUpperCase()}${key.slice(1)}`}
-            description={value}
-          />
-        ))}
-      <DetailEntryOptions />
-    </ContentContainer>
+    <Box flexDirection="column">
+      <ContentContainer>
+        {Object.entries(detailedEntry)
+          .filter(([key]) => key !== "downloadUrls")
+          .map(([key, value], idx) => (
+            <DetailRow
+              key={idx}
+              label={key === "id" ? key.toUpperCase() : `${key[0].toUpperCase()}${key.slice(1)}`}
+              description={value}
+            />
+          ))}
+        <DetailEntryOptions />
+      </ContentContainer>
+      <UsageInfo />
+    </Box>
   );
 };
 
--- src/tui/layouts/result-list/index.tsx
+++ src/tui/layouts/result-list/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFocus } from "ink";
+import { Box, useFocus } from "ink";
 import { RESULT_LIST_ACTIVE_LIST_INDEX } from "../../../constants";
 import ResultListItemOption from "./ResultListItemOption";
 import ResultListItemEntry from "./ResultListItemEntry";
@@ -8,6 +8,7 @@ import ContentContainer from "../../components/ContentContainer";
 import { useScrollableListControls } from "../../hooks/useScrollableListControls";
 import { useAppContext } from "../../contexts/AppContext";
 import { getRenderedListItems } from "../../../utils";
+import UsageInfo from "../../components/UsageInfo";
 
 const ResultList: React.FC = () => {
   const { anyEntryExpanded, activeExpandedListLength, listItems, setListItems } = useAppContext();
@@ -16,25 +17,28 @@ const ResultList: React.FC = () => {
   const renderedItems = getRenderedListItems(listItems, anyEntryExpanded, activeExpandedListLength);
 
   return (
-    <ContentContainer>
-      {renderedItems.map((item, index) =>
-        item.type === ResultListItemType.Option ? (
-          <ResultListItemOption
-            key={item.data.id}
-            item={item}
-            isActive={index === RESULT_LIST_ACTIVE_LIST_INDEX}
-          />
-        ) : (
-          <ResultListItemEntry
-            key={item.data.id}
-            item={item}
-            isActive={index === RESULT_LIST_ACTIVE_LIST_INDEX}
-            isExpanded={index === RESULT_LIST_ACTIVE_LIST_INDEX && anyEntryExpanded}
-            isFadedOut={index !== RESULT_LIST_ACTIVE_LIST_INDEX && anyEntryExpanded}
-          />
-        )
-      )}
-    </ContentContainer>
+    <Box flexDirection="column">
+      <ContentContainer>
+        {renderedItems.map((item, index) =>
+          item.type === ResultListItemType.Option ? (
+            <ResultListItemOption
+              key={item.data.id}
+              item={item}
+              isActive={index === RESULT_LIST_ACTIVE_LIST_INDEX}
+            />
+          ) : (
+            <ResultListItemEntry
+              key={item.data.id}
+              item={item}
+              isActive={index === RESULT_LIST_ACTIVE_LIST_INDEX}
+              isExpanded={index === RESULT_LIST_ACTIVE_LIST_INDEX && anyEntryExpanded}
+              isFadedOut={index !== RESULT_LIST_ACTIVE_LIST_INDEX && anyEntryExpanded}
+            />
+          )
+        )}
+      </ContentContainer>
+      <UsageInfo />
+    </Box>
   );
 };
 
