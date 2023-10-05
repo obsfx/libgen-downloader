446cefd93cbd59ae8dfea8c62ca530e9f76a23f5
omercanbalandi <balandiomer@gmail.com>
2023-10-06 00:08:05 +0300

    New gh-pages

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-lurkdown --files=README.md \
-  --styles='font.css,new.min.css,night.css' \
-  --titles='libgen-downloader'
-
-mv README.html index.html
-
-git add -A
-git commit -m "index"
-git push -u origin gh-pages
